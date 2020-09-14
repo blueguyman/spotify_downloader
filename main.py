0c4253b0a2c1bf8068e87de994c383634281c9b8
blueguyman <midhun128@users.noreply.github.com>
2020-09-14 08:14:16 +0300

    Fixed timeout before process start + Minor Changes

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from youtube_search import YoutubeSearch
 
 import api_keys
 
-TIMEOUT = 30
+TIMEOUT = 15
 SAVE_FOLDER = "download"
 DEBUG = False
+LOCK = mp.Lock()
 
 
 def main():
@@ -38,39 +39,34 @@ def main():
 
 
 def downloader(sp, playlist_id):
-
     tracks = get_playlist_tracks(sp, playlist_id)
-
-    lock = mp.Lock()
 
     track_queue = mp.Queue()
     yt_link_queue = mp.Queue()
     mp4_file_queue = mp.Queue()
     mp3_file_queue = mp.Queue()
 
-    pr_queue_tracks = mp.Process(target=queue_tracks, args=(lock, tracks, track_queue))
+    pr_queue_tracks = mp.Process(target=queue_tracks, args=(tracks, track_queue))
     pr_queue_tracks.start()
 
     pr_queue_video_links = mp.Process(
-        target=get_video_links, args=(lock, track_queue, yt_link_queue, len(tracks))
+        target=get_video_links, args=(track_queue, yt_link_queue, len(tracks))
     )
     pr_queue_video_links.start()
 
     pr_queue_mp4_files = mp.Process(
         target=download_mp4_files,
-        args=(lock, yt_link_queue, mp4_file_queue, len(tracks)),
+        args=(yt_link_queue, mp4_file_queue, len(tracks)),
     )
     pr_queue_mp4_files.start()
 
     pr_queue_mp3_files = mp.Process(
         target=convert_mp4_files_to_mp3,
-        args=(lock, mp4_file_queue, mp3_file_queue, len(tracks)),
+        args=(mp4_file_queue, mp3_file_queue, len(tracks)),
     )
     pr_queue_mp3_files.start()
 
-    pr_tag_mp3s = mp.Process(
-        target=tag_mp3_files, args=(lock, mp3_file_queue, len(tracks))
-    )
+    pr_tag_mp3s = mp.Process(target=tag_mp3_files, args=(mp3_file_queue, len(tracks)))
     pr_tag_mp3s.start()
 
     pr_queue_tracks.join()
@@ -82,14 +78,12 @@ def downloader(sp, playlist_id):
     print("\nDownload completed.\n")
 
 
-def mp_print(
-    lock, *args, sep=" ", end="\n", file=sys.stdout, flush=False, debug_only=True
-):
+def mp_print(*args, sep=" ", end="\n", file=sys.stdout, flush=False, debug_only=True):
     if debug_only and not DEBUG:
         return
-    lock.acquire()
+    LOCK.acquire()
     print(*args, sep=sep, end=end, file=file, flush=flush)
-    lock.release()
+    LOCK.release()
 
 
 def get_playlist_tracks(sp, playlist_id):
@@ -120,26 +114,27 @@ def format_track(track):
     return formatted_track
 
 
-def queue_tracks(lock, tracks, output_queue):
-    mp_print(lock, f"Found {len(tracks)} tracks in playlist.\n", debug_only=False)
+def queue_tracks(tracks, output_queue):
+    mp_print(f"Found {len(tracks)} tracks in playlist.\n", debug_only=False)
     for track in tracks:
         output_queue.put(track)
     output_queue.close()
 
 
-def get_video_links(lock, track_queue, output_queue, total_tracks):
+def get_video_links(track_queue, output_queue, total_tracks):
     tracks_found = 0
+    while track_queue.empty():
+        pass
     while True:
         try:
             track = track_queue.get(timeout=TIMEOUT)
 
             query = f"{', '.join(track['artists'])} - {track['title']} Lyrics"
-            mp_print(lock, "INFO:", f"Searching for '{query}'")
+            mp_print("INFO:", f"Searching for '{query}'")
 
             video = YoutubeSearch(query, max_results=1).videos[0]
             tracks_found += 1
             mp_print(
-                lock,
                 "INFO:",
                 f"Found '{video['title']}' at https://www.youtube.com/watch?v={video['id']}",
                 f"[{tracks_found}/{total_tracks}]",
@@ -151,12 +146,14 @@ def get_video_links(lock, track_queue, output_queue, total_tracks):
         except queue.Empty:
             break
 
-    mp_print(lock, "COMPLETED:", f"Found {tracks_found} tracks on YouTube")
+    mp_print("COMPLETED:", f"Found {tracks_found} tracks on YouTube")
     output_queue.close()
 
 
-def download_mp4_files(lock, yt_link_queue, output_queue, total_tracks):
+def download_mp4_files(yt_link_queue, output_queue, total_tracks):
     tracks_downloaded = 0
+    while yt_link_queue.empty():
+        pass
     while True:
         try:
             track, video_info = yt_link_queue.get(timeout=TIMEOUT)
@@ -167,11 +164,10 @@ def download_mp4_files(lock, yt_link_queue, output_queue, total_tracks):
                     yt = YouTube(f"http://youtube.com/watch?v={video_info['id']}")
                     video = yt.streams.filter(subtype="mp4")[0]
 
-                    mp_print(lock, "INFO:", f"Downloading '{video_info['title']}'")
+                    mp_print("INFO:", f"Downloading '{video_info['title']}'")
                     filepath = video.download(os.path.abspath("temp"))
                     tracks_downloaded += 1
                     mp_print(
-                        lock,
                         "INFO:",
                         f"Finished downloading '{video_info['title']}'",
                         f"[{tracks_downloaded}/{total_tracks}]",
@@ -182,7 +178,6 @@ def download_mp4_files(lock, yt_link_queue, output_queue, total_tracks):
                     tries += 1
                     if tries >= 3:
                         mp_print(
-                            lock,
                             "ERROR:",
                             f"An error occured during download of '{video_info['title']}':",
                             err,
@@ -194,13 +189,15 @@ def download_mp4_files(lock, yt_link_queue, output_queue, total_tracks):
         except queue.Empty:
             break
 
-    mp_print(lock, "COMPLETED:", f"Downloaded {tracks_downloaded} tracks from YouTube")
+    mp_print("COMPLETED:", f"Downloaded {tracks_downloaded} tracks from YouTube")
     output_queue.close()
 
 
-def convert_mp4_files_to_mp3(lock, mp4_file_queue, output_queue, total_tracks):
+def convert_mp4_files_to_mp3(mp4_file_queue, output_queue, total_tracks):
     tracks_converted = 0
     prevdir = os.getcwd()
+    while mp4_file_queue.empty():
+        pass
 
     while True:
         try:
@@ -215,12 +212,11 @@ def convert_mp4_files_to_mp3(lock, mp4_file_queue, output_queue, total_tracks):
 
             try:
                 with VideoFileClip(filepath) as video:
-                    mp_print(lock, "INFO:", f"Converting {filepath} to mp3")
+                    mp_print("INFO:", f"Converting {filepath} to mp3")
                     savepath = os.path.basename(filepath[:-1]) + "3"
                     video.audio.write_audiofile(savepath, verbose=False, logger=None)
                     tracks_converted += 1
                     mp_print(
-                        lock,
                         "INFO:",
                         f"Saved {savepath}",
                         f"[{tracks_converted}/{total_tracks}]",
@@ -239,13 +235,15 @@ def convert_mp4_files_to_mp3(lock, mp4_file_queue, output_queue, total_tracks):
         finally:
             os.chdir(prevdir)
 
-    mp_print(lock, "COMPLETED:", f"Converted {tracks_converted} tracks to mp3")
+    mp_print("COMPLETED:", f"Converted {tracks_converted} tracks to mp3")
     output_queue.close()
 
 
-def tag_mp3_files(lock, mp3_file_queue, total_tracks):
+def tag_mp3_files(mp3_file_queue, total_tracks):
     tracks_tagged = 0
     prevdir = os.getcwd()
+    while mp3_file_queue.empty():
+        pass
     while True:
         try:
             try:
@@ -268,13 +266,11 @@ def tag_mp3_files(lock, mp3_file_queue, total_tracks):
 
                 tracks_tagged += 1
                 mp_print(
-                    lock,
                     "INFO:",
                     f"Tagged file {path}",
                     f"[{tracks_tagged}/{total_tracks}]",
                 )
                 mp_print(
-                    lock,
                     f"Progress: {tracks_tagged}/{total_tracks} tracks. [{track['title']}]",
                     debug_only=False,
                 )
@@ -289,7 +285,7 @@ def tag_mp3_files(lock, mp3_file_queue, total_tracks):
         finally:
             os.chdir(prevdir)
 
-    mp_print(lock, "COMPLETED:", f"Tagged {tracks_tagged} mp3 files")
+    mp_print("COMPLETED:", f"Tagged {tracks_tagged} mp3 files")
 
 
 if __name__ == "__main__":
